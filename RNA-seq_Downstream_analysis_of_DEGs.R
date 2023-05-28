73c7855466b6d3a93416662abeaf55cd4204c9b5
FernandaVRom <fernanda.vargasromero@ucsf.edu>
2023-05-28 16:42:15 -0700

    Update RNA-seq_Downstream_analysis_of_DEGs.R

--- RNA-seq_Downstream_analysis_of_DEGs.R
+++ RNA-seq_Downstream_analysis_of_DEGs.R
@@ -47,22 +47,34 @@ heatm <- pheatmap(list1,
                   color=colorRampPalette(c("blue", "white", "red"))(20),
                   cluster_cols = TRUE,  
                   cutree_cols=3, 
-                  cutree_rows=2,
+                  cutree_rows=5,
                   show_rownames = FALSE
                   )
 
 ### Getting the order of the genes from top to bottom------------------------------------------------------------------------------
 
-order_genes <- rownames(list1[heatm$tree_row[["order"]],])
+order_genes <- rownames(list1)[heatm$tree_row[["order"]]]
 
-write.csv(order_genes, './Results/list_of_genes_heatmap_in_order.csv')
+# Create a data frame with the order and gene names
+order_data <- data.frame(order = 1:length(order_genes), ID = order_genes)
+
+# Save the data frame to a CSV file
+write.csv(order_data, './Results/list_of_genes_heatmap_in_order.csv', row.names = FALSE)
 
 ### Getting the genes that belong to each cluster --------------------------------------------------------------------------------
 
-# The k = 5 depends on how many clusters you want to observe on the heatmap. These should match the optimal number of clusters 
-# obtained earlier (see pipeline RNA-seq_quality_assessment_and_processing.Rmd for more)
+# Obtain the cluster assignments
 
-write.csv(sort(cutree(heatm$tree_row, k=5)), './Results/list_of_genes_heatmap_clusters.csv')
+clusters <- cutree(heatm$tree_row, k = 5)
+
+# The k = 5 depends on how many clusters you want to observe on the heatmap. 
+
+# Create a data frame with gene names and cluster assignments
+cluster_data <- data.frame(ID = rownames(list1), Cluster = clusters)
+
+# Save the data frame to a CSV file
+write.csv(cluster_data, "./Results/list_of_genes_heatmap_clusters.csv", row.names = FALSE)
+
 
 ### Save the heatmap into pdf ----------------------------------------------------------------------------------------------------
 
@@ -84,24 +96,19 @@ save_pheatmap_pdf(heatm, './Results/heatmap_clusters.pdf')
 # each gene in each cluster along the heatmap
 
 ### Obtaining the order of each cluster along the map ----------------------------------------------------------------------------
-# STEP 1 in the .csv files name the columns of genes as "ID"
-
-# STEP 2:load your files in R in the next order
+# Load your files in R in the next order
 
 a1 <- read.csv('./Results/list_of_genes_heatmap_clusters.csv')
-
 head(a1)
 
 a2 <- read.csv('./Results/list_of_genes_heatmap_in_order.csv')
-
-head(a2)
+head(a2) 
 
 #merge a1 into a2 and assign them by ID
 a3 <- merge(a1, a2, by ="ID")
-
 head(a3)
 
-#save the file (function needs to be completed)
+#save the file 
 write.csv(a3, './Results/list_of_genes_heatmap_in_order_and_by_cluster.csv')
 
 
@@ -135,108 +142,102 @@ write.csv(a3, './Results/list_of_genes_heatmap_in_order_and_by_cluster.csv')
 library(clusterProfiler)
 library(enrichplot)
 library(AnnotationDbi)
-
-
-# Load the annotation you will use
-# SET THE DESIRED ORGANISM HERE (in this case human)
-organism = "org.Hs.eg.db"
-
-library(organism, character.only = TRUE)
+library(org.Hs.eg.db)
+library(ggplot2)
 
 # Prepare INPUT 
 # Conversion of ENSGID to ENTREZ ID 
 
-keytypes(org.Hs.eg.db) #HELPFUL to see the diffent keys
+# Load your file with LFC and ENSEMBL IDs
+data <- read.csv('./Data/list_of_genes_LFC_for_GO_ENSEMBL.csv')
 
-a1 <- read.delim('./Data/list_of_genes_LFC_for_GO_ENSEMBL.csv', sep = ",")
+# Convert ENSEMBL IDs to ENTREZ IDs
+entrez_ids <- mapIds(org.Hs.eg.db,
+                     keys = data$ENSGID,
+                     column = "ENTREZID",
+                     keytype = "ENSEMBL")
 
-head(a1)
+# Create a new data frame with LFC and ENTREZ IDs
+new_data <- data.frame(ENTREZID = entrez_ids, LFC_Condition_A = data$Condition_A)
 
-# You need to specifically tell to keys which column to look into
-ENTREZID.list <- mapIds(org.Hs.eg.db, keys = a1$ENSGID, keytype="ENSEMBL", column = "ENTREZID")
+# Remove the ENSGID column
+new_data$ENSGID <- NULL
 
-head(ENTREZID.list)
+# Remove rows with missing or "NA" ENTREZ IDs
+new_data <- new_data[!is.na(new_data$ENTREZID), ]
 
+# Reorder the columns (optional)
+new_data <- new_data[, c("ENTREZID", "LFC_Condition_A")]
 
-# Make a new dataframe so that your ENTREZID goes next to your ENSGID
-a2 <- data.frame(a1$ENSGID, ENTREZID.list)
-
-head(a2)
-
-# Save it and transfer the ENTREZ to your DEG table on excel 
-write.csv(a2,'./Results/list_of_genes_for_GO_ENSEMBL_to_ENTREZ.csv', row.names = FALSE)
-
-# In excel generate a new list of genes that replaces the ENSEMBLID for the ENTREZID you obtained in lane 167.
-# An example will be included in the data folder once the work is ready for publication.
+# Write the new file with LFC and ENTREZ IDs
+write.csv(new_data, './Results/list_of_genes_ENTREZID_LFC.csv', row.names = FALSE)
 
 
 # Loading data with ENTREZID + LFC 
 
-df <- read.csv('./Data/list_of_genes_LFC_for_GO_ENTREZ.csv', header=TRUE)
+Gene_list <- read.csv('./Results/list_of_genes_ENTREZID_LFC.csv', header = TRUE)
 
-head(df)
+# Filter upregulated genes (LFC > 0)
+upregulated_genes <- Gene_list[Gene_list$LFC_Condition_A > 0, ]
 
-# name the vector
-names(original_gene_list) <- df$ENTREZID.list
+# Filter downregulated genes (LFC < 0)
+downregulated_genes <- Gene_list[Gene_list$LFC_Condition_A < 0, ]
 
-# omit any NA values (eliminate values not the names)
-gene_list<-na.omit(original_gene_list)
-
-# to omit the NA values and the refered gene name
-gene_list <- gene_list[!is.na(names(gene_list))]
-
-length(gene_list)
-
-head(gene_list)
-
-
-
-# GO over-representation analysis 
-## Function --------------------------------------------------------------------
-
-
-GO_results <- enrichGO(gene          = names(gene_list),
-                       OrgDb         = "org.Hs.eg.db",
-                       keyType       = "ENTREZID",
-                       ont           = "BP",
-                       pAdjustMethod = "BH",
-                       minGSSize = 20, 
-                       maxGSSize = 200, 
-                       pvalueCutoff  = 0.05,
-                       qvalueCutoff  = 0.05,
-                       readable      = TRUE)
-
-head(GO_results)
-
-write.csv(GO_results,'./Results/GO_result.csv')
-
-## Output ----------------------------------------------------------------------
-
-#Dotplot:
-#dotplot will show you the GO categories classified in your list of interest
-
-dotp <- dotplot(GO_results, 
-                color = "p.adjust",
-                showCategory = 10,
-                size = NULL,
-                split = NULL,
-                font.size = 10,
-                orderBy = "x",
-                label_format = 30,)
+# Perform GO over-representation analysis for upregulated genes
+upregulated_GO_results <- enrichGO(gene          = upregulated_genes$ENTREZID,
+                                   OrgDb         = org.Hs.eg.db,
+                                   keyType       = "ENTREZID",
+                                   ont           = "BP",
+                                   pAdjustMethod = "BH",
+                                   minGSSize     = 20, 
+                                   maxGSSize     = 200, 
+                                   pvalueCutoff  = 0.05,
+                                   qvalueCutoff  = 0.05,
+                                   readable      = TRUE
+                                   )
+# Save GO results for upregulated genes in a .csv file
+write.csv(upregulated_GO_results,'./Results/GO_result_upregulated.csv')
 
 
-save_dotplot_pdf <- function(x, filename, width=10, height=7) {
-  stopifnot(!missing(x))
-  stopifnot(!missing(filename))
-  pdf(filename, width=width, height=height)
-  grid::grid.newpage()
-  grid::grid.draw(x$gtable)
-  dev.off()
-}
+# Perform GO over-representation analysis for downregulated genes
+downregulated_GO_results <- enrichGO(gene          = downregulated_genes$ENTREZID,
+                                   OrgDb         = org.Hs.eg.db,
+                                   keyType       = "ENTREZID",
+                                   ont           = "BP",
+                                   pAdjustMethod = "BH",
+                                   minGSSize     = 20, 
+                                   maxGSSize     = 200, 
+                                   pvalueCutoff  = 0.05,
+                                   qvalueCutoff  = 0.05,
+                                   readable      = TRUE
+                                   )
 
-# Save the file in pdf
-save_dotplot_pdf(dotp, './Results/GO_result.pdf')
+# Save GO results for downregulated genes in a .csv file
+write.csv(downregulated_GO_results,'./Results/GO_result_downregulated.csv')
 
+# Visualize GO enrichment for upregulated genes
+Dotp_up <- dotplot(upregulated_GO_results, 
+        color = "p.adjust",
+        showCategory = 10,
+        size = NULL,
+        split = NULL,
+        font.size = 10,
+        orderBy = "x",
+        label_format = 30,
+        )
 
+# Visualize GO enrichment for downregulated genes
+Dotp_down <- dotplot(downregulated_GO_results, 
+        color = "p.adjust",
+        showCategory = 10,
+        size = NULL,
+        split = NULL,
+        font.size = 10,
+        orderBy = "x",
+        label_format = 30,
+        )
 
+.# Save the files in pdf
+ggsave(filename = "./Results/GO_result_upregulated.pdf", plot = Dotp_up, width = 10, height = 7)
+ggsave(filename = "./Results/GO_result_downregulated.pdf", plot = Dotp_down, width = 10, height = 7)
 
